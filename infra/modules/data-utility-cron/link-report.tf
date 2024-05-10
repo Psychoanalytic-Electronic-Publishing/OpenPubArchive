4ec8a8f3a08c18c6d95e6643c65d5ce99e5093f5
Jordan Allen <jordan@jordanallen.org>
2024-05-10 18:18:51 +0100

    Generate link report every Friday at 03:00 UTC
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

---
+++ infra/modules/data-utility-cron/link-report.tf
@@ -0,0 +1,28 @@
+resource "aws_cloudwatch_event_rule" "weekly_link_report" {
+  count = var.env == "production" ? 1 : 0
+
+  name                = "${var.stack_name}-weekly-link-report-${var.env}"
+  description         = "Run ${var.env} state machine every Friday at 03:00 UTC"
+  schedule_expression = "cron(0 3 ? * 6 *)"
+}
+
+resource "aws_cloudwatch_event_target" "weekly_link_report_target" {
+  count = var.env == "production" ? 1 : 0
+
+  rule     = aws_cloudwatch_event_rule.weekly_link_report[0].name
+  role_arn = aws_iam_role.allow_cloudwatch_to_execute_role.arn
+  arn      = var.state_machine_arn
+  input = jsonencode({
+    "task" : [
+      [
+        {
+          "limitRam" : false,
+          "directory" : "opasDataLoader",
+          "utility" : "opasDataLinker",
+          "args" : "--verbose --report-links 7"
+        }
+      ]
+    ]
+  })
+}
+
